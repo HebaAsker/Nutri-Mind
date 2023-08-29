4b48a0fe93d71d4ecd4846e861050238a9585f0e
SalmaRedaBedair <salmaredab173@gmail.com>
2023-08-30 01:27:47 +0300

    make some updates and test the DoctorWorkTimeController and DoctorWorkTime Request

---
+++ app/Http/Requests/DoctorWorkTimeRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DoctorWorkTimeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'date' => 'required|date',
+            'doctor_id' => 'required|integer|exists:doctors,id',
+            'available_times' => 'array',
+            'available_times.*' => 'string|regex:/^[0-2][0-9]:[0-5][0-9]$/|unique:doctor_work_times,date,time'
+        ];
+    }
+    public function messages(): array
+    {
+        return [
+            'date.required' => 'The date field is required.',
+            'date.date' => 'Please enter a valid date.',
+            'doctor_id.required' => 'Please select doctor.',
+            'doctor_id.integer' => 'Unauthorized Access.',
+            'doctor_id.exists' => 'The selected doctor does not exist.',
+            'available_times.array' => 'The available times must be an array.',
+            'available_times.*.string' => 'Each available time must be a string.',
+            'available_times.*.regex' => 'Invalid time format. Please enter a time in the format HH:MM.',
+            'available_times.*.unique' => 'The selected time slot is already taken for the specified date.',
+        ];
+    }
+}
