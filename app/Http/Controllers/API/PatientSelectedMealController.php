52a6c4eca812067b0860c5e6ce006b2394f954ba
SalmaRedaBedair <salmaredab173@gmail.com>
2023-08-31 00:27:00 +0300

    create  controller of patient saved and selected meal

--- app/Http/Controllers/API/PatientSelectedMealController.php
+++ app/Http/Controllers/API/PatientSelectedMealController.php
@@ -2,18 +2,21 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Http\Requests\PatientMealRequest;
 use App\Models\PatientSelectedMeal;
+use App\Traits\GeneralTrait;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 class PatientSelectedMealController extends Controller
 {
+    use GeneralTrait;
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        return $this->getData($request,'App\Models\PatientSelectedMeal');
     }
 
     /**
@@ -27,15 +30,17 @@ class PatientSelectedMealController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(PatientMealRequest $request)
     {
-        //
+        $validated=$request->validated();
+        PatientSelectedMeal::create($request->all());
+        return $this->returnSuccess('Meal added successfully.');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(PatientSelectedMeal $patientSelectedMeal)
+    public function show(PatientSelectedMeal $PatientSelectedMeal)
     {
         //
     }
@@ -43,7 +48,7 @@ class PatientSelectedMealController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(PatientSelectedMeal $patientSelectedMeal)
+    public function edit(PatientSelectedMeal $PatientSelectedMeal)
     {
         //
     }
@@ -51,16 +56,18 @@ class PatientSelectedMealController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, PatientSelectedMeal $patientSelectedMeal)
+    public function update(PatientMealRequest $request, PatientSelectedMeal $PatientSelectedMeal)
     {
-        //
+        $validated=$request->validated();
+        $PatientSelectedMeal->update($request->all());
+        return $this->returnSuccess('Meal updated successfully.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(PatientSelectedMeal $patientSelectedMeal)
+    public function destroy($PatientSelectedMealId)
     {
-        //
+        return $this->destroyData($PatientSelectedMealId,'App\Models\PatientSelectedMeal','patient_selected_meals');
     }
 }
