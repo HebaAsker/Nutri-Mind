74570f84df28634c22646da2bb685e45875221be
SalmaRedaBedair <salmaredab173@gmail.com>
2023-09-02 10:54:42 +0300

    make some updates to Report

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -7,27 +7,25 @@ use App\Http\Requests\ReportRequest;
 use App\Models\Appointment;
 use App\Models\Report;
 use App\Traits\GeneralTrait;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class ReportController extends Controller
 {
     use GeneralTrait;
 
-    // store and update
-    // appointment_id, disease, description => request
-    // done in report page
     public function store(ReportRequest $request)
     {
         $validated = $request->validated();
-        $report = Report::where('appointment_id',$request->appointment_id)->first();
-        if (empty($report)) {
-            Report::create($request->all());
-            return $this->returnSuccess('Report added successfully.');
-        } else {
-            $report->update($request->all());
-            return $this->returnSuccess('Report updated successfully.');
-        }
+        Report::create($request->all());
+        return $this->returnSuccess('Report added successfully.');
+    }
+
+    public function update(ReportRequest $request, $reportId)
+    {
+        $validated = $request->validated();
+        $report=Report::find($reportId);
+        $report->update($request->only(['diagnosis_of_his_state','description']));
+        return $this->returnSuccess('Report updated successfully.');
     }
 
     // used to see report of specific appointment
@@ -43,23 +41,18 @@ class ReportController extends Controller
         if ($validator->fails()) {
             return $this->returnError($validator->errors());
         }
-        $report = Appointment::join('doctor_work_times', 'appointments.doctor_work_time_id', '=', 'doctor_work_times.id')
+        $report = Appointment::join('doctor_set_times', 'appointments.doctor_set_time_id', '=', 'doctor_set_times.id')
             ->join('patients', 'appointments.patient_id', '=', 'patients.id')
             ->join('reports', 'appointments.id', '=', 'reports.appointment_id')
             ->orderBy('date')
-            ->where('appointments.id',$appointmentId)
+            ->where('appointments.id', $appointmentId)
             ->first([
                 "full_name",
                 "age",
                 "diagnosis_of_his_state",
                 "description",
                 "appointment_id"
-    ]);
-        return $this->returnData('report',$report);
-    }
-
-    public function destroy($reportId)
-    {
-        return $this->destroyData($reportId, 'App\Models\Report', 'reports');
+            ]);
+        return $this->returnData('report', $report);
     }
 }
